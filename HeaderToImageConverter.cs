258ef5ae21e91d1832b67eccaf449582f398ece2
Trppyxd <33310226+Trppyxd@users.noreply.github.com>
2019-03-13 22:20:06 +0200

    Initial commit

---
+++ HeaderToImageConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace TreeView
+{
+    /// <summary>
+    /// Converts a full path to specific image type of a drive, folder or file
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // get the full path
+            var path = (string) value;
+
+            // if the path is null, ignore
+            if (path == null)
+                return null;
+
+            var name = MainWindow.GetFileFolderName(path);
+
+            // By default we presume an image
+            var image = "Images/file.png";
+
+            // If the name is blank, we presume its a drive as we cannot have a blank file or folder name
+            if (string.IsNullOrEmpty(name))
+                image = "Images/drive.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/folder-closed.png";
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
